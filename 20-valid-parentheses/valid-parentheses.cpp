aa27dff5df686d672ef7e829527b8c1441716fd9
praveenraja23 <2kdaydreamer23@gmail.com>
2026-09-20 11:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (88.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(char c:s){
-            if(c=='('||c=='['||c=='{'){
-                 st.push(c);
-                
+        stack <char> st;
+        for(char  ch:s){
+            if(ch=='('||ch=='{'||ch=='['){
+              st.push(ch);
             }
             else{
                 if(st.empty()){
                     return false;
                 }
                 char top=st.top();
-                if((c==')' && top!='(')||(c==']'&& top!='[')||(c=='}'&& top!='{')){
+    
+                if((ch==')'&&  top!='(')||(ch=='}'&& top!='{')||(ch==']'&& top!='[')){
                     return false;
                 }
                 st.pop();
-            }
-        }
-        return st.empty();
+             } }
+            return st.empty();
     }
 };
